9e66fceb0155702d9a63bf6d838758ff70541339
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-27 16:26:07 +0530

    Xcoded Updated Changes

---
+++ ResponsiveUI3-SwiftUI/View/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  ResponsiveUI3-SwiftUI
+//
+//  Created by Nimish Mothghare on 27/04/25.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
